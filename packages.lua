6b636929bdea0cea32af5d2f332445c4bc86e3e2
xdoxx123 <162730079+xdoxx123@users.noreply.github.com>
2026-03-27 18:38:40 +0000

    Update packages.lua

--- packages.lua
+++ packages.lua
@@ -1,3 +1,31 @@
 return {
-  {name="test",files={"/test.lua"="bd:/bin/test.lua"}}
+  ["skid"] = {
+    name="poo",
+    files={["main/bob.lua"]="bd:/bin"}
+  },
+  ["linknet"] = {
+    name="linknet",
+    files={["main/linknets/linknet.lua"]="/usr/bin"}
+  },
+  ["protnet"] = {
+    name="protnet",
+    files={["main/linknets/protnet.lua"]="/usr/bin"}
+  },
+  ["pm"] = {
+    name="pm",
+    files={["main/pm.lua"]="/usr/bin"}
+  },
+  ["shit"] = {
+    name="shit lib",
+    files={["main/shit.lua"]="/usr/lib"}
+  },
+  ["shiter"] = {
+    name="shiter",
+    files={["main/shiter.lua"]="/usr/bin"},
+    depends={"shit"}
+  },
+  ["daemon"] = {
+    name="deamon maker",
+    files={["main/daemon.lua"]="/usr/bin"}
+  }
 }
